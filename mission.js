e7c731da6554a5b7ceacb989a53c6c1915b546e1
Seth Roberts <sethsoar315@gmail.com>
2025-05-03 23:48:26 -0600

    fixed dark image logo

--- mission.js
+++ mission.js
@@ -7,7 +7,7 @@ themeSelector.addEventListener('change', changeTheme);
         
         if (themeSelector.value === 'dark') {
             body.classList.add('dark');
-            logo.src = 'byui-logo_white.webp';
+            logo.src = 'byui-logo_white.png';
         } else {
             body.classList.remove('dark');
             logo.src = 'byui-logo_blue.webp';
