c38444a9f51d6cbf9090185eace2e150401ba479
Seth Roberts <sethsoar315@gmail.com>
2025-05-03 23:40:55 -0600

    added dark theme

---
+++ mission.js
@@ -0,0 +1,15 @@
+const themeSelector = document.getElementById('display-select');
+themeSelector.addEventListener('change', changeTheme);
+    function changeTheme() {
+        const body = document.body;
+        const logo = document.querySelector('footer img');
+
+        
+        if (themeSelector.value === 'dark') {
+            body.classList.add('dark');
+            logo.src = 'byui-logo_white.webp';
+        } else {
+            body.classList.remove('dark');
+            logo.src = 'byui-logo_blue.webp';
+        }
+    }
