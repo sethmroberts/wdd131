f98ba5d8ccda256ef5e3b34978e76598e66a38a8
Seth Roberts <sethsoar315@gmail.com>
2025-06-14 16:39:58 -0600

    register

---
+++ register/register.js
@@ -0,0 +1,86 @@
+let participantCount = 1;
+
+function participantTemplate(count) {
+  return `
+    <section class="participant${count}">
+      <p>Participant ${count}</p>
+      <div class="item">
+        <label for="fname${count}"> First Name<span>*</span></label>
+        <input id="fname${count}" type="text" name="fname${count}" required />
+      </div>
+      <div class="item activities">
+        <label for="activity${count}">Activity #<span>*</span></label>
+        <input id="activity${count}" type="text" name="activity${count}" />
+      </div>
+      <div class="item">
+        <label for="fee${count}">Fee ($)<span>*</span></label>
+        <input id="fee${count}" type="number" name="fee${count}" />
+      </div>
+      <div class="item">
+        <label for="date${count}">Desired Date <span>*</span></label>
+        <input id="date${count}" type="date" name="date${count}" />
+      </div>
+      <div class="item">
+        <p>Grade</p>
+        <select name="grade${count}">
+          <option selected disabled></option>
+          <option value="1">1st</option>
+          <option value="2">2nd</option>
+          <option value="3">3rd</option>
+          <option value="4">4th</option>
+          <option value="5">5th</option>
+          <option value="6">6th</option>
+          <option value="7">7th</option>
+          <option value="8">8th</option>
+          <option value="9">9th</option>
+          <option value="10">10th</option>
+          <option value="11">11th</option>
+          <option value="12">12th</option>
+        </select>
+      </div>
+    </section>
+  `;
+}
+
+const addButton = document.getElementById("add");
+
+addButton.addEventListener("click", () => {
+  participantCount++;
+  const html = participantTemplate(participantCount);
+  addButton.insertAdjacentHTML("beforebegin", html);
+});
+
+const form = document.querySelector("form");
+
+form.addEventListener("submit", submitForm);
+
+function submitForm(event) {
+  event.preventDefault();
+
+  const adultName = document.getElementById("adult_name").value;
+  const total = totalFees();
+  const participants = document.querySelectorAll("[id^=fee]").length;
+
+  const summary = document.getElementById("summary");
+  form.style.display = "none";
+  summary.innerHTML = successTemplate({ name: adultName, count: participants, total: total });
+}
+
+function totalFees() {
+  let feeElements = document.querySelectorAll("[id^=fee]");
+  feeElements = [...feeElements];
+
+  let total = feeElements.reduce((sum, el) => {
+    return sum + Number(el.value || 0);
+  }, 0);
+
+  return total;
+}
+
+function successTemplate(info) {
+  return `
+    <h2>Success!</h2>
+    <p>Thank you ${info.name} for registering.</p>
+    <p>You have registered ${info.count} participant(s) and owe $${info.total} in fees.</p>
+  `;
+}
